026940b6be41e81c404cc51f136dc78e30cbb6d3
Hygor Dias <hygor.christian@gmail.com>
2023-01-27 08:46:03 -0800

    Add loader

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,11 +1,16 @@
+/* eslint-disable @typescript-eslint/require-await */
 import FeatureList from 'components/FeatureList';
+import Head from 'next/head';
 import { featureResources } from 'services/themoviedb/types';
 
-export const revalidate = 10;
+export const revalidate = 30;
 
-export default function MoviesPage() {
+export default async function MoviesPage() {
   return (
     <main>
+      <Head>
+        <title>hello</title>
+      </Head>
       <FeatureList
         title="My top 10"
         resource={featureResources.movies_favorites}
